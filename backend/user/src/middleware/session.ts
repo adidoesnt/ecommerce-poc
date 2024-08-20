767480558e4b1e332f27668560f8b20122241158
Banerjee Aditya <aditya.banerjee@activate.sg>
2024-08-20 16:09:16 +0800

    use session store

--- backend/user/src/middleware/session.ts
+++ backend/user/src/middleware/session.ts
@@ -2,11 +2,21 @@ import session from 'express-session';
 import type { Express } from 'express';
 import { Logger } from 'utils';
 import config from 'config.json';
+import MongoStore from 'connect-mongo';
 
-const { SESSION_SECRET = 'dummy-secret' } = process.env;
+const {
+    SESSION_SECRET = 'dummy-secret',
+    MONGODB_URI = 'dummy-uri',
+    SESSION_COLLECTION_NAME = 'passport',
+} = process.env;
 
 const logger = new Logger({
     module: 'middleware/session',
+});
+
+const sessionStore = new MongoStore({
+    mongoUrl: MONGODB_URI,
+    collectionName: SESSION_COLLECTION_NAME,
 });
 
 export const setupSession = (app: Express) => {
@@ -14,6 +24,7 @@ export const setupSession = (app: Express) => {
     const sessionOptions = {
         secret: SESSION_SECRET,
         ...config.session,
+        store: sessionStore,
     };
     app.use(session(sessionOptions));
 };
